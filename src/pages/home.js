872299876e671a45980f7e3003eeaa7e0a167e78
Md. Rayhan Islam Sefat <112571481+RayhanSefat@users.noreply.github.com>
2024-10-05 23:13:47 +0600

    created the icon

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Icon from "../templates/icon";
+
+class Home extends React.Component {
+    render() {
+        return(
+            <>
+                <Icon />
+                <p>This is Home</p>
+            </>
+        );
+    }
+} 
+
+export default Home;
---
+++ src/templates/icon.js
@@ -0,0 +1,15 @@
+import React from "react"
+import './icon.css'
+
+class Icon extends React.Component {
+    render() {
+        return(
+            <div class='icon'>
+                <img class='logo' src='img/so_logo.png' alt='logo'/>
+                Stack Overflow
+            </div>
+        );
+    }
+}
+
+export default Icon;
