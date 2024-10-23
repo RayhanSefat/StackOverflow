a5f03c188241ff5288b8f2bea9b152522aaea51f
Md. Rayhan Islam Sefat <112571481+RayhanSefat@users.noreply.github.com>
2024-10-23 21:11:37 +0600

    Home page will be different for guest users and authenticated users.

--- src/Frontend/pages/home/home.js
+++ src/Frontend/pages/home/home.js
@@ -26,7 +26,7 @@ const Home = () => {
           err.response &&
           (err.response.status === 401 || err.response.status === 403)
         ) {
-          navigate("/signin");
+          setUsername(""); // Ensure username is reset if not logged in
         } else {
           setError("Error fetching user information.");
         }
@@ -43,43 +43,48 @@ const Home = () => {
     };
 
     fetchUsername();
-    fetchPosts();
-  }, [navigate]);
+    if (username) {
+      fetchPosts();
+    }
+  }, [navigate, username]);
 
   const handleSaveContent = async (e) => {
     e.preventDefault();
     setError("");
     setMessage("");
-  
+
     if (!description || !content || !extension) {
       setError("Please fill in all fields.");
       return;
     }
-  
+
     try {
       const response = await axios.post("http://localhost:5000/save_content", {
         description,
         content,
         extension,
       });
-  
+
       setMessage(response.data.message);
       setDescription("");
       setContent("");
       setExtension("txt");
-  
+
       // Fetch the updated posts after saving content
       const postsResponse = await axios.get("http://localhost:5000/posts");
       setPosts(postsResponse.data.posts);
     } catch (err) {
-      if (err.response && (err.response.status === 401 || err.response.status === 403)) {
+      if (
+        err.response &&
+        (err.response.status === 401 || err.response.status === 403)
+      ) {
         navigate("/signin");
       } else {
         setError("Error occurred while saving content.");
       }
     }
   };
-  
+
   const handleSignOut = async () => {
     try {
       await axios.post("http://localhost:5000/signout");
@@ -92,68 +97,79 @@ const Home = () => {
   return (
     <div className="home">
       <Helmet>
-        <title>Welcome, {username}!</title>
+        <title>{username ? `Welcome, ${username}!` : "Home"}</title>
       </Helmet>
+      <Icon />
       <main>
-        <div>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-        <div>
-          <Link to="/signin">Sign In</Link>
-        </div>
-        <Icon />
-        <h1>Hello, {username}!</h1>
-        <form onSubmit={handleSaveContent} className="content-form">
-          <div className="form-group">
-            <label htmlFor="description">Description:</label>
-            <textarea
-              id="description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="content">Paste Content:</label>
-            <textarea
-              id="content"
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="extension">Select File Extension:</label>
-            <select
-              id="extension"
-              value={extension}
-              onChange={(e) => setExtension(e.target.value)}
-            >
-              <option value="txt">.txt</option>
-              <option value="c">.c</option>
-              <option value="cpp">.cpp</option>
-              <option value="css">.css</option>
-              <option value="js">.js</option>
-              <option value="py">.py</option>
-            </select>
-          </div>
-          <button type="submit">Save Content</button>
-        </form>
-        {error && <p className="error-message">{error}</p>}
-        {message && <p className="success-message">{message}</p>}
-        <h2>Recent Posts:</h2>{" "}
-        <div className="post-list">
-          {" "}
-          {posts.map((post, index) => (
-            <div key={index} className="post-item">
-              {" "}
-              <h3>{post.username}</h3>{" "}
-              <p className="post-description">{post.description}</p>{" "}
-              <pre className="file-content">{post.content}</pre>{" "}
-              <small>{new Date(post.timestamp).toLocaleString()}</small>{" "}
+        {(username === "Guest") ? (
+          <div>
+            <div>
+              <Link to="/signup">Sign Up</Link>
             </div>
-          ))}{" "}
-        </div>{" "}
-        <button onClick={handleSignOut}>Sign Out</button>
-      </main>{" "}
+            <div>
+              <Link to="/signin">Sign In</Link>
+            </div>
+          </div>
+        ) : (
+          <>
+            <h1>Hello, {username}!</h1>
+            <form onSubmit={handleSaveContent} className="content-form">
+              <div className="form-group">
+                <label htmlFor="description">Description:</label>
+                <textarea
+                  id="description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="content">Paste Content:</label>
+                <textarea
+                  id="content"
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="extension">Select File Extension:</label>
+                <select
+                  id="extension"
+                  value={extension}
+                  onChange={(e) => setExtension(e.target.value)}
+                >
+                  <option value="txt">.txt</option>
+                  <option value="c">.c</option>
+                  <option value="cpp">.cpp</option>
+                  <option value="css">.css</option>
+                  <option value="js">.js</option>
+                  <option value="py">.py</option>
+                </select>
+              </div>
+              <button type="submit">Save Content</button>
+            </form>
+            {error && <p className="error-message">{error}</p>}
+            {message && <p className="success-message">{message}</p>}
+            <h2>Recent Posts:</h2>
+            <div className="post-list">
+              {posts.map((post, index) => (
+                <div key={index} className="post-item">
+                  <h3>{post.username}</h3>
+                  <p className="post-description">{post.description}</p>
+                  <pre className="file-content">{post.content}</pre>
+                  <small>
+                    {new Date(
+                      new Date(post.timestamp).setHours(
+                        new Date(post.timestamp).getHours() - 6
+                      )
+                    ).toLocaleString()}
+                  </small>
+                </div>
+              ))}
+            </div>
+            <button onClick={handleSignOut}>Sign Out</button>
+          </>
+        )}
+      </main>
     </div>
   );
 };
